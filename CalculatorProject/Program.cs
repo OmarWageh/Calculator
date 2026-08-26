2289fea3fc800882c97fd0223838701709928296
omar wageh <omarwagih815@gmail.com>
2026-08-26 14:10:51 +0300

    feat: initialize calculator project

---
+++ CalculatorProject/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Welcome To My Calculator Project .");
+Console.Write("Please Enter the First Number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("pleae Enter the Second Number: ");
+int num2 = int.Parse(Console.ReadLine());
